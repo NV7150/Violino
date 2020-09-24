eb588d3cb5cddc82065e8fcbfbcbce2fd6aa8319
NV7150 <7150macNV777DAI@gmail.com>
2020-09-24 15:58:48 +0900

    方向入力にフレーム単位で猶予を設定できるように

--- Assets/Scripts/Judge/ButtonInput.cs
+++ Assets/Scripts/Judge/ButtonInput.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 
 namespace Judge {
     public class ButtonInput : MonoBehaviour, RhythmInput {
+        [SerializeField] private int affordanceFrames = 5;
+        
         [SerializeField] private string rightButtonName = "Right";
         [SerializeField] private string centerButtonName = "Center";
         [SerializeField] private string leftButtonName = "Left";
@@ -15,10 +17,44 @@ namespace Judge {
 
         private readonly Dictionary<NoteLane, string> _laneButtons = new Dictionary<NoteLane, string>();
 
+        private NoteDirection _currDirection = NoteDirection.NONE;
+
+        private int _noneCount = 0;
+
         void Awake() {
             _laneButtons.Add(NoteLane.RIGHT, rightButtonName);
             _laneButtons.Add(NoteLane.LEFT, leftButtonName);
             _laneButtons.Add(NoteLane.CENTER, centerButtonName);
+        }
+
+        private void Update() {
+            updateDir();
+        }
+
+        void updateDir() {
+            var dir = getCurrDir();
+            if (dir != NoteDirection.NONE) {
+                _currDirection = dir;
+                _noneCount = 0;
+                return;
+            }
+
+            if (_noneCount >= affordanceFrames) {
+                _currDirection = NoteDirection.NONE;
+            } else {
+                _noneCount++;
+            }
+        }
+
+        NoteDirection getCurrDir() {
+            //とりあえず現状、同時に押された場合は考慮しない
+            if (rightDirButtonNames.Any(Input.GetButton)) {
+                return NoteDirection.RIGHT;
+            } else if(leftDirButtonNames.Any(Input.GetButton)){
+                return NoteDirection.LEFT;
+            }
+
+            return NoteDirection.NONE;
         }
 
         public bool isJudgeTiming(NoteLane lane) {
@@ -30,14 +66,7 @@ namespace Judge {
         }
 
         public NoteDirection getDirection() {
-            //とりあえず現状、同時に押された場合は考慮しない
-            if (rightDirButtonNames.Any(Input.GetButton)) {
-                return NoteDirection.RIGHT;
-            } else if(leftDirButtonNames.Any(Input.GetButton)){
-                return NoteDirection.LEFT;
-            }
-
-            return NoteDirection.NONE;
+            return _currDirection;
         }
     }
 }
